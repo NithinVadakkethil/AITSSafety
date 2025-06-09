dcfcecdb16ad01bc9fb8290a3fca546c590eeda8
nithin@zinfog.com <nithinvadakkethil18@gmail.com>
2025-06-09 19:32:26 +0530

    changes

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import './global.css'
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View className='flex-1 items-center justify-center'>
+      <Text>App</Text>
+    </View>
+  )
+}
+
+export default App
--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,23 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
+const { withNativeWind } = require("nativewind/metro");
+const { wrapWithReanimatedMetroConfig } = require("react-native-reanimated/metro-config");
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('@react-native/metro-config').MetroConfig}
- */
-const config = {};
+const defaultConfig = getDefaultConfig(__dirname);
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+// Modify the resolver to support SVG
+defaultConfig.resolver.assetExts = defaultConfig.resolver.assetExts.filter(ext => ext !== "svg");
+defaultConfig.resolver.sourceExts.push("svg");
+
+// Add transformer for SVG support
+defaultConfig.transformer.babelTransformerPath = require.resolve("react-native-svg-transformer");
+
+// Merge configurations
+let config = mergeConfig(defaultConfig, {});
+
+// Wrap with NativeWind
+config = withNativeWind(config, { input: "./global.css" });
+
+// Wrap with Reanimated
+config = wrapWithReanimatedMetroConfig(config);
+
+module.exports = config;
